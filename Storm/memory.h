ad194a90759556aef4ae7b857800a878827b3010
Amrf000 <1138824393@qq.com>
2024-03-31 22:30:14 +0800

    annother try

--- Storm/memory.h
+++ Storm/memory.h
@@ -4,12 +4,25 @@
 #include <cstdint>
 #include <cstdlib>
 
-void* SMemAlloc(size_t bytes, const char* filename, int32_t linenumber, uint32_t flags);
+void *SMemAlloc(size_t bytes, const char *filename, int32_t linenumber, uint32_t flags);
 
-void SMemFree(void* ptr);
+void SMemFree(void *ptr);
 
-void SMemFree(void* ptr, const char* filename, int32_t linenumber, uint32_t flags);
+void SMemFree(void *ptr, const char *filename, int32_t linenumber, uint32_t flags);
 
-void* SMemReAlloc(void* ptr, size_t bytes, const char* filename, int32_t linenumber, uint32_t flags);
+void *SMemReAlloc(void *ptr, size_t bytes, const char *filename, int32_t linenumber, uint32_t flags);
 
+
+namespace Blizzard {
+    namespace Memory {
+
+// Functions
+        void *Allocate(uint32_t bytes);
+
+        void *Allocate(uint32_t bytes, uint32_t flags, const char *filename, uint32_t linenumber, const char *a5);
+
+        void Free(void *ptr);
+
+    } // namespace Memory
+} // namespace Blizzard
 #endif
